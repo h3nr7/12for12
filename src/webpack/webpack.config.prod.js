de3cfc69f1eaae3e5fcc019c91a1669fcd50f234
Henry YP Ho <henryyp@monkiki.co>
2020-04-25 23:37:36 +0100

    updated

--- src/webpack/webpack.config.prod.js
+++ src/webpack/webpack.config.prod.js
@@ -59,7 +59,7 @@ module.exports = {
             },
             {
                 test: /\.(jpe?g|gif|png|svg)$/i,
-                loader: ["url-loader?limit=10000", "file-loader"],
+                loader: "url-loader?limit=10000",
             },
             {
                 test: /\.json$/,
